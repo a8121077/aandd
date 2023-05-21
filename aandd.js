b4d06d83a9338df036bfcc51f353e76b00b50a3d
Daiki Kanamori <a8121077@gsuite.si.aoyama.ac.jp>
2023-05-21 22:36:10 +0900

    prac4

--- aandd.js
+++ aandd.js
@@ -28,3 +28,91 @@ function createRandomNumbers(array, n) {
         array.push(Math.floor(Math.random() * n));
     }
 }
+
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
+    }
+}
+
+function mistakebinarysearch(array, target) {
+    var nowarray = array
+    var x = 1
+    var newarray = []
+    var answer = -1
+    while (nowarray.length != 1) {
+        var mid = Math.round(nowarray.length / 2)
+        var newlen = nowarray.length - mid
+        console.log(nowarray)
+        if (target > nowarray[mid]) {
+            for (var i = 0; i < nowarray.length - mid; i++) {
+                newarray.push(nowarray[i + mid])
+            }
+        } else if (target < nowarray[mid]) {
+            for (var i = 0; i < mid; i++) {
+                newarray.push(nowarray[i])
+            }
+        } else {
+            answer = mid;
+            break
+        }
+        nowarray = newarray
+        newarray = []
+        x = x + 1
+    }
+    var html = ''
+    if (answer == - 1) {
+        html = html + '<table border="1"><tr>'
+        html = html + '<td>表の中にありません</td>'
+        html = html + '</tr></table>'
+    } else {
+        html = html + '<table border="1"><tr>'
+        html = html + '<td>' + answer + '</td>'
+        html = html + '</tr></table>'
+    }
+    document.getElementById('ans').innerHTML = html
+}
+
+function binarysearch(array, target) {
+    var mid = Math.round(array.length / 2)
+    var max = array.length
+    var min = 0
+    var n = 2
+    var answer = -1
+    if (array[min] == target) {
+        answer = min
+    } else {
+        while (max - min > 1) {
+            if (target > array[mid]) {
+                min = mid
+                mid = Math.round(mid + (array.length / 2 ** n))
+                n = n + 1
+            } else if (target < array[mid]) {
+                max = mid
+                mid = Math.round(mid - (array.length / 2 ** n))
+                n = n + 1
+            } else {
+                answer = mid
+                break
+            }
+        }
+    }
+    var html = ''
+    if (answer == - 1) {
+        html = html + '<table border="1"><tr>'
+        html = html + '<td>表の中にありません</td>'
+        html = html + '</tr></table>'
+    } else {
+        html = html + '<table border="1"><tr>'
+        html = html + '<td>' + answer + '</td>'
+        html = html + '</tr></table>'
+    }
+    document.getElementById('ans').innerHTML = html
+}
